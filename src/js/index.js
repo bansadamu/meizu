8d23b3f96f10a58e23148815d66ab6047118c583
bansadamu <bansadamu@gmail.com>
2021-01-12 15:51:49 +0800

    v1.12.15

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,18 @@ $.ajax({
     url: "../../interface/getData.php",
     dataType: "json",
     success: function (res) {
-        console.log(res);
+        let temp = '';
+        res.forEach((elm,i)=>{
+            let picture = JSON.parse(elm.picture);
+            temp += `<li class="other-img">
+            <a href="./product1.html?id=${elm.id}"><img class="lazy" data-original="${picture[0].src}" alt=""></a>
+            <div>
+              <p>${elm.title}</p>
+              <p>博物馆级健康光 RG0 舒适低蓝光 磁吸随贴安装</p>
+              <p>￥${elm.price}</p>
+            </div>
+          </li>`;
+        });
+        $('.product-list>._project').append(temp);
     }
 });
